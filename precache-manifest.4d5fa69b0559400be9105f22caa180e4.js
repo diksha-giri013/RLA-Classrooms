06252ca60fcc75f943fe27fb26fc9c4d9513ec54
Aditya6571 <adityagiri657@yahoo.in>
2020-02-23 01:46:51 +0530

    Updates

---
+++ precache-manifest.4d5fa69b0559400be9105f22caa180e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd8ae0420b5b6fca9d49848ab2831623",
+    "url": "/RLA-Classrooms/index.html"
+  },
+  {
+    "revision": "6936a96d38a300c1b983",
+    "url": "/RLA-Classrooms/static/css/main.085f1b03.chunk.css"
+  },
+  {
+    "revision": "c4c84d34344f8c2a6811",
+    "url": "/RLA-Classrooms/static/js/2.c0dfb198.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/RLA-Classrooms/static/js/2.c0dfb198.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6936a96d38a300c1b983",
+    "url": "/RLA-Classrooms/static/js/main.130b9ca4.chunk.js"
+  },
+  {
+    "revision": "7454e5281917d28355a3",
+    "url": "/RLA-Classrooms/static/js/runtime-main.46b294f6.js"
+  }
+]);
